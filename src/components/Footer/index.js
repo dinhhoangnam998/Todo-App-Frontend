e63069a00f86b20d238e7c7cb165e5ac3e718715
Dinh Hoang Nam <dinhhoangnam998@gmail.com>
2021-11-15 22:38:45 +0700

    :pencil2: setup project struct

---
+++ src/components/Footer/index.js
@@ -0,0 +1,3 @@
+export default function Footer({ props }) {
+  return <div>This app is created by Hoang Nam!</div>;
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+export default function Header({ props }) {
+  return (
+    <div>
+      <h1>Welcome to my to do app!</h1>
+    </div>
+  );
+}
